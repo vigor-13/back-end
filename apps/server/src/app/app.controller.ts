92590ac5611e6f27f592d0fcd2c88f3dbec58515
vigor-13 <vigor13.dev@gmail.com>
2022-05-06 18:05:55 +0900

    feat: add configuration module to server-app

--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/mode')
+  getMode(): string {
+    const mode = this.appService.getMode();
+    return `MODE: ${mode}`;
+  }
 }
--- apps/server/src/app/app.service.ts
+++ apps/server/src/app/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getMode(): string {
+    const mode = this.configService.get<string>('mode');
+    console.log(mode);
+    return mode;
   }
 }
