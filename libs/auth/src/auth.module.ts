2c42a352ad710512a61ca255b922aae016e28a16
vigor-13 <vigor13.dev@gmail.com>
2022-05-26 15:13:03 +0900

    feat: change auth module to global scope

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -1,13 +1,14 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controllers';
 import { JwtStrategy, LocalStrategy } from './providers';
 import { LocalAuthService } from './services';
 
+@Global()
 @Module({})
 export class AuthModule {
-  static register(): DynamicModule {
+  static forRoot(): DynamicModule {
     return {
       module: AuthModule,
       imports: [
@@ -16,7 +17,7 @@ export class AuthModule {
       ],
       controllers: [AuthController],
       providers: [LocalAuthService, LocalStrategy, JwtStrategy],
-      exports: [LocalAuthService],
+      exports: [LocalAuthService, JwtModule],
     };
   }
 }
